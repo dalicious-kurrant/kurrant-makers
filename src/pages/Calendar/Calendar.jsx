e61a03b94faacbb7cb48b79a694d620fab610a85
KyoungTae <kyoungtae.jang@dalicious.co>
2023-02-18 13:40:58 +0900

    일정관리 페이지 퍼블리싱 완료

--- src/pages/Calendar/Calendar.jsx
+++ src/pages/Calendar/Calendar.jsx
@@ -6,142 +6,180 @@ import CalendarDetail from './components/CalendarDetail';
 import CalendarSimple from './components/CalendarSimple';
 const makersCalendar = [
   {
-    "presetMakersId" : 1,
-    "schaduleStatus" : 0,
-    "serviceDate" : "2023-02-24",
-    "diningType": "아침",
-    "makersCapa" : 100,
-    "leftMakersCapa" : 80,
-    "deadline" : "2023/02/30 18:00:00",
-    "clientSchadule" : [
-       {
-           "pickupTime" : "07:50",
-           "clientName" : "달리셔스",
-           "clientCapa" : 20,
-           "foodSchadule" : [
-                {
-                    "presetFoodId" : 9,
-                    "food" : "음식",
-                    "foodStatus" : "판매중",
-                    "foodCapa" : 100,
-                },
-                {
-                  "presetFoodId" : 10,
-                  "food" : "음식",
-                  "foodStatus" : "판매중",
-                  "foodCapa" : 100,
-              }
-           ]
-       }
-    ]
-},
-{
-  "presetMakersId" : 2,
-  "schaduleStatus" : 1,
-  "serviceDate" : "2023-02-24",
-  "diningType": "아침",
-  "makersCapa" : 100,
-  "deadline" : "2023/02/30 18:00:00",
-  "clientSchadule" : [
-     {
-         "pickupTime" : "07:50",
-         "clientName" : "달리셔스",
-         "clientCapa" : 20,
-         "foodSchadule" : [
-              {
-                  "presetFoodId" : 1,
-                  "food" : "음식",
-                  "foodStatus" : "판매중",
-                  "foodCapa" : 100,
-                  "schaduleStatus" : 0,
-              },
-              {
-                "presetFoodId" : 8,
-                "food" : "음식",
-                "foodStatus" : "판매중",
-                "foodCapa" : 100,
-                "schaduleStatus" : 0,
-            }
-         ]
-     }
-  ]
-},
-{
-  "presetMakersId" : 3,
-  "schaduleStatus" : 0,
-  "serviceDate" : "2023-02-24",
-  "diningType": "아침",
-  "makersCapa" : 100,
-  "deadline" : "2023/02/30 18:00:00",
-  "clientSchadule" : [
-     {
-         "pickupTime" : "07:50",
-         "clientName" : "달리셔스",
-         "clientCapa" : 20,
-         "foodSchadule" : [
-              {
-                  "presetFoodId" : 2,
-                  "food" : "음식",
-                  "foodStatus" : "판매중",
-                  "foodCapa" : 100,
-                  "schaduleStatus" : 0,
-              },
-              {
-                "presetFoodId" : 3,
-                "food" : "음식",
-                "foodStatus" : "판매중",
-                "foodCapa" : 100,
-                "schaduleStatus" : 0,
-            },
-            {
-              "presetFoodId" : 4,
-              "food" : "음식",
-              "foodStatus" : "판매중",
-              "foodCapa" : 100,
-              "schaduleStatus" : 0,
-          }
-         ]
-     }
-  ]
-},
-{
-  "presetMakersId" : 4,
-  "schaduleStatus" : 2,
-  "serviceDate" : "2023-02-24",
-  "diningType": "아침",
-  "makersCapa" : 100,
-  "deadline" : "2023/02/30 18:00:00",
-  "clientSchadule" : [
-     {
-         "pickupTime" : "07:50",
-         "clientName" : "달리셔스",
-         "clientCapa" : 20,
-         "foodSchadule" : [
-              {
-                  "presetFoodId" : 5,
-                  "food" : "음식",
-                  "foodStatus" : "판매중",
-                  "foodCapa" : 100,
-                  "schaduleStatus" : 0,
-              },
-              {
-                "presetFoodId" : 6,
-                "food" : "음식",
-                "foodStatus" : "판매중",
-                "foodCapa" : 100,
-                "schaduleStatus" : 0,
-            },
-            {
-              "presetFoodId" : 7,
-              "food" : "음식",
-              "foodStatus" : "판매중",
-              "foodCapa" : 100,
-              "schaduleStatus" : 0,
-          }
-         ]
-     }
-  ]
-}
+    presetMakersId: 1,
+    schaduleStatus: 0,
+    serviceDate: '2023-02-21',
+    diningType: '아침',
+    makersCapa: 100,
+    leftMakersCapa: 80,
+    deadline: '2023/02/30 18:00:00',
+    clientSchadule: [
+      {
+        pickupTime: '07:50',
+        clientName: '달리셔스',
+        clientCapa: 20,
+        foodSchadule: [
+          {
+            presetFoodId: 9,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+          },
+          {
+            presetFoodId: 10,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    presetMakersId: 2,
+    schaduleStatus: 1,
+    serviceDate: '2023-02-22',
+    diningType: '아침',
+    makersCapa: 100,
+    deadline: '2023/02/30 18:00:00',
+    clientSchadule: [
+      {
+        pickupTime: '07:50',
+        clientName: '달리셔스',
+        clientCapa: 20,
+        foodSchadule: [
+          {
+            presetFoodId: 1,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+          {
+            presetFoodId: 8,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    presetMakersId: 3,
+    schaduleStatus: 0,
+    serviceDate: '2023-02-23',
+    diningType: '아침',
+    makersCapa: 100,
+    deadline: '2023/02/30 18:00:00',
+    clientSchadule: [
+      {
+        pickupTime: '07:50',
+        clientName: '달리셔스',
+        clientCapa: 20,
+        foodSchadule: [
+          {
+            presetFoodId: 2,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+          {
+            presetFoodId: 3,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+          {
+            presetFoodId: 4,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    presetMakersId: 5,
+    schaduleStatus: 0,
+    serviceDate: '2023-02-23',
+    diningType: '점심',
+    makersCapa: 100,
+    deadline: '2023/02/30 18:00:00',
+    clientSchadule: [
+      {
+        pickupTime: '07:50',
+        clientName: '달리셔스',
+        clientCapa: 20,
+        foodSchadule: [
+          {
+            presetFoodId: 2,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+          {
+            presetFoodId: 3,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+          {
+            presetFoodId: 4,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+        ],
+      },
+    ],
+  },
+  {
+    presetMakersId: 4,
+    schaduleStatus: 2,
+    serviceDate: '2023-02-24',
+    diningType: '아침',
+    makersCapa: 100,
+    deadline: '2023/02/30 18:00:00',
+    clientSchadule: [
+      {
+        pickupTime: '07:50',
+        clientName: '달리셔스',
+        clientCapa: 20,
+        foodSchadule: [
+          {
+            presetFoodId: 5,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+          {
+            presetFoodId: 6,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+          {
+            presetFoodId: 7,
+            food: '음식',
+            foodStatus: '판매중',
+            foodCapa: 100,
+            schaduleStatus: 0,
+          },
+        ],
+      },
+    ],
+  },
 ];
 
 const Calendar = () => {
@@ -150,16 +188,15 @@ const Calendar = () => {
   const [page, setPage] = useState(false);
   const [actives, setActives] = useState([]);
   const [active, setActive] = useState([]);
-  
-  const [testData, setTestData] = useState(makersCalendar);
 
+  const [testData, setTestData] = useState(makersCalendar);
 
   useEffect(() => {
     setCount(
       makersCalendar.map((v, i) => {
         let num = 0;
         v.clientSchadule.map((s, si) => {
-          return  s.foodSchadule.map((d, di) => {
+          return s.foodSchadule.map((d, di) => {
             return num++;
           });
         });
@@ -167,68 +204,96 @@ const Calendar = () => {
       }),
     );
   }, []);
-  
-  
+
   return (
     <PageWrapper>
       <Wrapper>
         <TitleBox>일정관리</TitleBox>
         <HeaderBox>
           <ViewTypeBox>
-        <Button
-          toggle
-          color={"facebook"}
-          active={false}
-          size={'huge'}
-          onClick={()=>{setPage(true)}}>
-          간편보기
-        </Button>
-        <Button
-          toggle
-          color={"grey"}
-          active={false}
-          size={'huge'}
-          onClick={()=>{setPage(false)}}>
-           상세보기
-        </Button>
-        </ViewTypeBox>
-        <AccessBox>
-        <Button
-          toggle
-          color={"green"}
-          active={false}
-          size={'huge'}
-          onClick={()=>{
-            setTestData(testData.map((makers)=>{
-              return {...makers,schaduleStatus:1,clientSchadule:makers.clientSchadule.map((client)=>{
-                return{...client,foodSchadule:client.foodSchadule.map((food)=>{
-                  return {...food , schaduleStatus:1}
-                })}
-              })}
-            }))
-          }}>
-          전체승인
-        </Button>
-        <Button
-          toggle
-          color={"red"}
-          active={false}
-          size={'huge'}
-          onClick={()=>{
-            setTestData(testData.map((makers)=>{
-              return {...makers,schaduleStatus:2,clientSchadule:makers.clientSchadule.map((client)=>{
-                return{...client,foodSchadule:client.foodSchadule.map((food)=>{
-                  return {...food , schaduleStatus:2}
-                })}
-              })}
-            }))
-          }}>
-           전체거절
-        </Button>
-        </AccessBox>
+            <Button
+              toggle
+              color={page ? 'grey' : 'facebook'}
+              active={false}
+              size={'huge'}
+              onClick={() => {
+                setPage(false);
+              }}>
+              간편보기
+            </Button>
+            <Button
+              toggle
+              color={page ? 'facebook' : 'grey'}
+              active={false}
+              size={'huge'}
+              onClick={() => {
+                setPage(true);
+              }}>
+              상세보기
+            </Button>
+          </ViewTypeBox>
+          <AccessBox>
+            <Button
+              toggle
+              color={'green'}
+              active={false}
+              size={'huge'}
+              onClick={() => {
+                setTestData(
+                  testData.map(makers => {
+                    return {
+                      ...makers,
+                      schaduleStatus: 1,
+                      clientSchadule: makers.clientSchadule.map(client => {
+                        return {
+                          ...client,
+                          foodSchadule: client.foodSchadule.map(food => {
+                            return {...food, schaduleStatus: 1};
+                          }),
+                        };
+                      }),
+                    };
+                  }),
+                );
+              }}>
+              전체승인
+            </Button>
+            <Button
+              toggle
+              color={'red'}
+              active={false}
+              size={'huge'}
+              onClick={() => {
+                setTestData(
+                  testData.map(makers => {
+                    return {
+                      ...makers,
+                      schaduleStatus: 2,
+                      clientSchadule: makers.clientSchadule.map(client => {
+                        return {
+                          ...client,
+                          foodSchadule: client.foodSchadule.map(food => {
+                            return {...food, schaduleStatus: 2};
+                          }),
+                        };
+                      }),
+                    };
+                  }),
+                );
+              }}>
+              전체거절
+            </Button>
+          </AccessBox>
         </HeaderBox>
-        {page ? <CalendarDetail count={count} testData={testData} setTestData={setTestData}/> 
-        :<CalendarSimple testData={testData} setTestData={setTestData}/>}
+        {page ? (
+          <CalendarDetail
+            count={count}
+            testData={testData}
+            setTestData={setTestData}
+          />
+        ) : (
+          <CalendarSimple testData={testData} setTestData={setTestData} />
+        )}
       </Wrapper>
     </PageWrapper>
   );
@@ -251,16 +316,11 @@ const TitleBox = styled.div`
   font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
   padding: 20px;
 `;
-const ViewTypeBox = styled.div`
-
-`;
-const AccessBox = styled.div`
-  
-`;
+const ViewTypeBox = styled.div``;
+const AccessBox = styled.div``;
 const HeaderBox = styled.div`
   display: flex;
   justify-content: space-between;
   padding-top: 20px;
   padding-bottom: 20px;
-  
-`
+`;
