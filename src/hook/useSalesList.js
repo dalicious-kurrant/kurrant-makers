16be703e5e5d2055b80d783e41548a6d5f6ed71c
KyoungTae <kyoungtae.jang@dalicious.co>
2023-03-08 00:04:55 +0900

    자동 최신화

--- src/hook/useSalesList.js
+++ src/hook/useSalesList.js
@@ -2,7 +2,15 @@ import {useQuery} from 'react-query';
 import {salesApis} from '../api/sales';
 
 export function useGetSalesList(startDate, endDate, diningSelect) {
-  return useQuery('salesList', () => {
-    return salesApis.loadSalesList(startDate, endDate, diningSelect);
-  });
+  console.log('기간별 판매내역 최신화');
+  return useQuery(
+    'salesList',
+    () => {
+      return salesApis.loadSalesList(startDate, endDate, diningSelect);
+    },
+    {
+      refetchInterval: 20000,
+      staleTime: 5 * 1000,
+    },
+  );
 }
