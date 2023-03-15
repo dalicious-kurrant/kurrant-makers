778f7843e84962f6f4e3d7c7c9a6ccbf382e2a20
jihye <jihye.kim@dalicious.co>
2023-03-15 17:06:08 +0900

    .

--- src/pages/SalesCalendar/Schedule.jsx
+++ src/pages/SalesCalendar/Schedule.jsx
@@ -187,7 +187,7 @@ const Schedule = () => {
                               key={spot.spotId + spot.spotName + i + l + idx}
                               style={{
                                 marginRight: 10,
-                                marginTop: 10,
+                                // marginTop: 10, flex-wrap 적용할때 활성화
                               }}>
                               <Label
                                 content={`스팟: ${spot.spotId}`}
@@ -305,7 +305,6 @@ const ButtonWrap = styled.div`
 const TableWrap = styled.div`
   display: flex;
   margin-right: 5px;
-  flex-wrap: wrap;
 `;
 const BoldText = styled.span`
   font-weight: 700;
