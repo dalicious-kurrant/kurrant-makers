39301eb95f2b4afb9ba21ab62823c59e78d588d9
dalicious-manager <hello@dalicious.co>
2023-08-31 09:05:33 +0900

    url 변경

--- src/api/sales.js
+++ src/api/sales.js
@@ -3,6 +3,6 @@ import instance from '../Shared/axios';
 export const salesApis = {
   loadSalesList: async (startDate, endDate, diningSelect) =>
     await instance.get(
-      `/makers/orders?startDate=${startDate}&endDate=${endDate}&diningTypes=${diningSelect}`,
+      `/makers/orders/deliveies?startDate=${startDate}&endDate=${endDate}&diningTypes=${diningSelect}`,
     ),
 };
